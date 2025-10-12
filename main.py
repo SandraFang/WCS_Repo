1090088ec93e79dd56cb547c8c8b478c3b242f83
SandraFang <sandrajf222@gmail.com>
2025-10-11 22:36:55 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+#Take the first input from the user and call it start_year.
+a= input ("start_year: ")
+print ("start_year:", a)
+#convert a to integer type
+a=int(a) 
+
+#Take the second input from the user and call it end_year.
+b= input ("end_year: ")
+print ("end_year:", b)
+#convert b to integer type
+b=int(b) 
+
+# Create a variable age and assign it to the calculated age from the two inputs.
+print ("the age variable:" , b-a) 
