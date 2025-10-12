36d1cafcf66919783adef3e94350fff937f431ca
SandraFang <sandrajf222@gmail.com>
2025-10-11 23:18:32 -0400

    Add files via upload

--- main1.py
+++ main1.py
@@ -1,6 +1,7 @@
+#Take the first input from the user and call it first_number.
 a= input ("first_number: ")
 print ("first_number: ", a)
-#convert a to int type
+#convert a to float type
 a=float(a) 
 
 #Take the second input from the user and call it second_number.
@@ -10,7 +11,15 @@ print ("second_number: ", b)
 b=float(b) 
 
 #Create the variables sum
-result=a+b
-print(f"The product is {result}")
+print ("the sum of a and b: " , a+b) 
+
+#Create the variables subtraction
+print ("the substraction of a and b: " , a-b) 
+
+#Create the variables multiplication
+print ("the multiplication of a and b: " , a*b) 
+
+#Create the variables division
+print ("the division of a and b: " , a/b) 
 
 
