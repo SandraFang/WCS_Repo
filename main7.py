201c0d81028e96db5cc83613c0ed69a02edc164c
SandraFang <sandrajf222@gmail.com>
2025-10-30 14:40:06 -0400

    Add files via upload

---
+++ main7.py
@@ -0,0 +1,26 @@
+arr =[328,2,3,11,9,81,17]
+mid=len(arr)//2
+left=arr[:mid]
+right=arr[mid:]
+left.sort()
+right.sort()
+i=0
+j=0
+k=0
+while i<len(left) and j<len(right):
+    if left[i]<right[j]:
+        arr[k]=left[i]
+        i+=1
+    else:
+        arr[k]=right[j]
+        j+=1
+    k+=1
+while i<len(left):
+    arr[k]=left[i]
+    i+=1
+    k+=1
+while j<len(right):
+    arr[k]=right[j]
+    j+=1
+    k+=1
+print ("Merge Sort:",arr)
